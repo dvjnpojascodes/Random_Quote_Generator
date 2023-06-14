e0f279c56012189d1507d7f525856f4aa66ffefc
bble_codes <deviejanepojas@gmail.com>
2023-06-14 22:36:38 +0800

    added functions to the console file

--- console.js
+++ console.js
@@ -1,3 +1,14 @@
 console.log ("hello world")
 console.log ("hello world")
 console.log ("hello world")
+
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const min = 1;
+const max = 10;
+const randomNumber = getRandomNumber(min, max);
+console.log(randomNumber);
+
